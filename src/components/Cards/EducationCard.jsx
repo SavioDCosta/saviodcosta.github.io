99ac26fa7a28865d627a861ca5f3638664a58fe4
SavioDCosta <savio.a.dcosta@gmail.com>
2024-03-10 14:41:18 -0700

    Modified portfolio content

--- src/components/Cards/EducationCard.jsx
+++ src/components/Cards/EducationCard.jsx
@@ -2,97 +2,97 @@ import React from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-    width: 650px;
-    border-radius: 10px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    padding: 12px 16px;
-    justify-content: space-between;
-    position: relative;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
-    transition: all 0.3s ease-in-out;
-    &:hover {
-        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
-        transform: translateY(-5px);
-    }
-    @media only screen and (max-width: 768px) {
-        padding: 10px;
-        gap: 8px;
-        width: 300px;
-    }
-    border: 0.1px solid #306ee8;
-    box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  width: 650px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  padding: 12px 16px;
+  justify-content: space-between;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+    transform: translateY(-5px);
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10px;
+    gap: 8px;
+    width: 300px;
+  }
+  border: 0.1px solid #306ee8;
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
 `;
 
 const Top = styled.div`
-    display: flex;
-    gap: 12px;
-    width: 100%;
+  display: flex;
+  gap: 12px;
+  width: 100%;
 `;
 
 const Logo = styled.img`
-    height: 50px;
-    background-color: #000;
-    border-radius: 10px;
-    margin-top: 4px;
-    @media only screen and (max-width: 768px) {
-        height: 40px;
-    }
+  height: 50px;
+  background-color: #fff;
+  border-radius: 10px;
+  margin-top: 4px;
+  @media only screen and (max-width: 768px) {
+    height: 40px;
+  }
 `;
 
 const Body = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
 
 const Name = styled.div`
-    font-size: 18px;
-    font-weight: 600;
-    color: ${({ theme }) => theme.text_primary + 99};
-    @media only screen and (max-width: 768px) {
-        font-size: 12px;
-    }
+  font-size: 18px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_primary + 99};
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+  }
 `;
 
 const Degree = styled.div`
-    font-size: 14px;
-    font-weight: 500;
-    color: ${({ theme }) => theme.text_secondary + 99};
-    @media only screen and (max-width: 768px) {
-        font-size: 12px;
-    }
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_secondary + 99};
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+  }
 `;
 
 const Date = styled.div`
-    font-size: 12px;
-    font-weight: 400;
-    color: ${({ theme }) => theme.text_secondary + 80};
-    @media only screen and (max-width: 768px) {
-        font-size: 10px;
-    }
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 80};
+  @media only screen and (max-width: 768px) {
+    font-size: 10px;
+  }
 `;
 
 const Description = styled.div`
-    width: 100%;
+  width: 100%;
+  font-size: 15px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_primary + 99};
+  margin-bottom: 10px;
+  pre {
+    /* Styles for the nested <pre> tag */
     font-size: 15px;
-    font-weight: 400;
-    color: ${({ theme }) => theme.text_primary + 99};
-    margin-bottom: 10px;
+    white-space: pre-wrap;
+    margin: 0; /* Reset margin for <pre> */
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
     pre {
-        /* Styles for the nested <pre> tag */
-        font-size: 15px;
-        white-space: pre-wrap;
-        margin: 0; /* Reset margin for <pre> */
+      font-size: 12px;
     }
-    @media only screen and (max-width: 768px) {
-        font-size: 12px;
-        pre {
-            font-size: 12px;
-        }
-    }
+  }
 `;
 
 // const Span = styled.span`
@@ -105,31 +105,31 @@ const Description = styled.div`
 // `;
 
 const Grade = styled.div`
-    font-size: 14px;
-    font-weight: 500;
-    color: ${({ theme }) => theme.text_secondary + 99};
-    @media only screen and (max-width: 768px) {
-        font-size: 12px;
-    }
+  font-size: 14px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_secondary + 99};
+  @media only screen and (max-width: 768px) {
+    font-size: 12px;
+  }
 `;
 
 const EducationCard = ({ education }) => {
-    return (
-        <Card>
-            <Top>
-                <Logo src={education.img}></Logo>
-                <Body>
-                    <Name>{education.school}</Name>
-                    <Degree>{education.degree}</Degree>
-                    <Date>{education.date}</Date>
-                </Body>
-            </Top>
-            <Grade>Grade: {education.grade}</Grade>
-            <Description>
-                <pre>{education.desc}</pre>
-            </Description>
-        </Card>
-    );
+  return (
+    <Card>
+      <Top>
+        <Logo src={education.img}></Logo>
+        <Body>
+          <Name>{education.school}</Name>
+          <Degree>{education.degree}</Degree>
+          <Date>{education.date}</Date>
+        </Body>
+      </Top>
+      <Grade>Grade: {education.grade}</Grade>
+      <Description>
+        <pre>{education.desc}</pre>
+      </Description>
+    </Card>
+  );
 };
 
 export default EducationCard;
